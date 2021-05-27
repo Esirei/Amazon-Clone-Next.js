0090178ed74a20f015c6a45c734f2e53cab87090
Esi're <esiri92@gmail.com>
2021-05-27 15:42:16 +0100

    load products serverside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Banner from '~/components/Banner';
 import Header from '~/components/Header';
 import ProductFeed from '~/components/ProductFeed';
+import { Product } from '~/features/product';
 
-export default function Home() {
+interface Props {
+  products: Product[];
+}
+
+export default function Home({ products }: Props) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -15,8 +22,16 @@ export default function Home() {
 
       <main className="max-w-screen-2xl mx-auto">
         <Banner />
-        <ProductFeed products={[]} />
+        <ProductFeed products={products} />
       </main>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const products = await axios
+    .get<Product[]>('https://fakestoreapi.com/products')
+    .then(response => response.data);
+
+  return { props: { products } };
+};
